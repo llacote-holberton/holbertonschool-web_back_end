1126f7f0a8b95c098521e1c4bb8b2dd1df2dc9a8
Laurent Lacôte <laurent.lacote@private.com>
2026-06-15 14:15:16 +0200

    WBE - Python Async -  Feat: T1 Sequential chaining of async calls
    NOTE: task instructions require specifically the use of
      asyncio.as_completed framed in a loop.

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Enhancing previous sleep example with ordered async repetition"""
+
+import random
+import asyncio
+from typing import List
+
+# Cannot work because of '-' character
+# from 0-basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """asynchronously wait up to max_delay seconds, n times"""
+    if not isinstance(n, int):
+        raise TypeError("n expected to be an int")
+    if n < 0:
+        raise ValueError("n expected to be >=  0")
+
+    chosen_delays = []
+    tasks = []
+    for i in range(0, n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+        # chosen_delays.append(task)
+    # THIS won't work as intended since it just returns a list of tasks
+    # await asyncio.wait(chosen_delays)
+    for task in asyncio.as_completed(tasks):
+        chosen_delays.append(await task)
+    return chosen_delays
+
+
+# ===== Task instructions =====
+# Import wait_random from the previous python file that you've written
+#   and write an async routine called wait_n that takes in 2 int arguments
+#   (in this order): n and max_delay.
+# You will spawn wait_random n times with the specified max_delay.
+# Wait_n should return the list of all the delays (float values).
+# The list of the delays should be in ascending order without using sort()
+#   because of concurrency.
