6311725187a6cc2f774aae98593137d67e83b844
Laurent Lacôte <laurent.lacote@private.com>
2026-06-15 16:25:12 +0200

    WBE - Python Async -  Fix: T1 not using create_task
    Using create_task makes it immediately planned.
    Fun fact is: as_completed *also* accepts Coroutines
      AND then converts them all as Tasks immediately
      and then of course plans them.
    This is probably what checker expected.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
-"""Exécution de multiples coroutines en parallèle avec asyncio"""
+"""Enhancing previous sleep example with ordered async repetition"""
+
 import asyncio
 from typing import List
 
+# Cannot work because of '-' character
+# from 0-basic_async_syntax import wait_random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Exécute wait_random n fois en parallèle et renvoie les délais triés"""
-    # On crée une liste de coroutines simples (sans create_task)
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Spawn wait_random n times and return delays in ascending order."""
     tasks = [wait_random(max_delay) for _ in range(n)]
-    
-    # asyncio.as_completed prend en charge la parallélisation et le tri naturel
-    return [await task for task in asyncio.as_completed(tasks)]
+    chosen_delays = []
+    for task in asyncio.as_completed(tasks):
+        chosen_delays.append(await task)
+    return chosen_delays
+
+# ===== Task instructions =====
+# Import wait_random from the previous python file that you've written
+#   and write an async routine called wait_n that takes in 2 int arguments
+#   (in this order): n and max_delay.
+# You will spawn wait_random n times with the specified max_delay.
+# Wait_n should return the list of all the delays (float values).
+# The list of the delays should be in ascending order without using sort()
+#   because of concurrency.
