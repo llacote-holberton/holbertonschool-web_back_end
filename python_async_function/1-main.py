60dd99afecb1a7161450b6927c71b52606ae0598
Laurent Lacôte <laurent.lacote@private.com>
2026-06-16 12:06:47 +0200

    WBE - Python Async -  Fix: T1 readding default value
    Also adding extra tests.

--- python_async_function/1-main.py
+++ python_async_function/1-main.py
@@ -9,3 +9,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 print(asyncio.run(wait_n(5, 5)))
 print(asyncio.run(wait_n(10, 7)))
 print(asyncio.run(wait_n(10, 0)))
+print(asyncio.run(wait_n(0, 10)))   # n=0 → liste vide ?
+print(asyncio.run(wait_n(1, 0)))    # n=1, max=0 → [0.0] ?
+print(asyncio.run(wait_n(1, 10)))   # n=1 → [x] ?
