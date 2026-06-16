f2fb5fea59a8b6b6c85ba0907bc516fde6ede5a3
Laurent Lacôte <laurent.lacote@private.com>
2026-06-16 11:49:59 +0200

    WBE - Python Async -  Fix: T1 REVERTING the "not use create_task"
    Makes Checker fail a "requirement" so was not this.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawn wait_random n times and return delays in ascending order."""
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     chosen_delays = []
     for task in asyncio.as_completed(tasks):
         chosen_delays.append(await task)
