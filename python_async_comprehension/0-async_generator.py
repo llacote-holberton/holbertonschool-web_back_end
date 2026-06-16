baebc334b05b3945155046bf8c1a770e56e37a0b
Laurent Lacôte <laurent.lacote@private.com>
2026-06-16 15:36:27 +0200

    WBE - PyAsynComp - Fix: T0 trying non-async Generator
    In case of...

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Provides a basic random number generator waiting between each pick"""
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Generates 10 rounds of 'sleep 1s and pick random float' instructions"""
     for _ in range(10):
         await asyncio.sleep(1)
